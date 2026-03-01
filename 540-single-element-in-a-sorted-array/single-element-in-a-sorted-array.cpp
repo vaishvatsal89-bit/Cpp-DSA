67209fb59313f09b51dbef9c777ecf409fcdf83f
vaishvatsal89-bit <vaishvatsal89@gmail.com>
2026-03-01 11:43:36 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (34.31%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int st = 0;
-        int end = nums.size() - 1;
-        int n = nums.size();
+        int st=0;
+        int n=nums.size();
+        int end=n-1;
 
-        while (st <= end) {
-            int mid = st + (end - st) / 2;
-
-            // If only one element
-            if (n == 1) return nums[0];
-
-            // Boundary cases
-            if (mid == 0) {
-                if (nums[0] != nums[1]) return nums[0];
-                else return nums[1];
-            }
-
-            if (mid == n - 1) {
-                if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
-                else return nums[n - 2];
-            }
-
-            // If mid is the single element
-            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
-                return nums[mid];
-
-            // Even index
-            if (mid % 2 == 0) {
-                if (nums[mid] == nums[mid + 1])
-                    st = mid + 1;
-                else
-                    end = mid - 1;
-            }
-            // Odd index
-            else {
-                if (nums[mid] == nums[mid - 1])
-                    st = mid + 1;
-                else
-                    end = mid - 1;
-            }
+        if(n==1)return nums[0];
+       
+       while(st<=end){
+        int mid=st+(end-st)/2;
+       
+        if(mid==0 && nums[0]!=nums[1]) return nums[mid];
+        if(mid==n-1 && nums[n-1]!=nums[n-2])return nums[mid];
+        if(nums[mid-1]!=nums[mid] && nums[mid]!=nums[mid+1]) return nums[mid];
+      
+        //left
+       if(mid%2==0){
+        if(nums[mid-1]==nums[mid]){
+            end=mid-1;
         }
-
-        return -1;
+        else{
+            st=mid+1;
+        }
+       }//right
+       else{
+        if(nums[mid-1]==nums[mid]){
+            st=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+       }
+       }
+       return -1;
     }
+    
 };
