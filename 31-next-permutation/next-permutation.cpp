462c8ffcff0744ff04f7217faca09201ca0586b1
vaishvatsal89-bit <vaishvatsal89@gmail.com>
2026-03-26 19:03:41 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (92.33%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& A) {
+        int piv=-1,n=A.size();
+        for(int i=n-2;i>=0;i--){
+            if(A[i]<A[i+1]){
+                piv=i;
+                break;
+            }
+        }
+         if(piv==-1){
+              reverse(A.begin(),A.end());
+              return;
+            }
+       for(int i=n-1;i>piv ;i--){
+         if(A[i]>A[piv]){
+            swap(A[i],A[piv]);
+            break;
+         }
+       }
+       int i=piv+1,j=n-1;
+       while(i<=j){
+        swap(A[i++],A[j--]);
+       }
+    }
+};
