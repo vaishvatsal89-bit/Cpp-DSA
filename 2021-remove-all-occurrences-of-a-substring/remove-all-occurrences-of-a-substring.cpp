93e0a11daed9c52801c547de545fcb6c6cbe7dde
vaishvatsal89-bit <vaishvatsal89@gmail.com>
2026-08-19 14:30:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (71.91%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,7 +1,7 @@
 class Solution {
     public:
         string removeOccurrences(string s, string part) {
-                while (s.length() > 0 && s.find(part) != string::npos) {
+                while (s.length() > 0 && s.find(part) <s.length()) {
                             s.erase(s.find(part), part.length());
                                     }
                                             return s;
